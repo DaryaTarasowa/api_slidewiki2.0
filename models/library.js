882aaf53602f8a71ff6099b92dc79f4149cb3a19
DaryaTarasowa <darya.tarasowa@gmail.com>
2015-01-12 15:35:38 +0100

    redundant function deleted from library.js

--- models/library.js
+++ models/library.js
@@ -35,16 +35,6 @@ var _ = require('underscore');
         });
         return cleaned;
     };
-    
-    exports.arrClean = function(actual) {        
-        var newArray = new Array();
-        for(var i = 0; i<actual.length; i++){
-            if (actual[i]){
-                newArray.push(actual[i]);
-            }
-        }
-        return newArray;
-    };
 
     
 
